ccc3d9b01d3920679659e240dcd07a10445b204e
oopss003 <jhpark8271@gmail.com>
2025-07-10 17:59:51 +0900

    Update NaverMap.swift
    "위치 찾기 성공"

--- NaverMap.swift
+++ NaverMap.swift
@@ -11,18 +11,22 @@
 //
 //  Created by 황성진 on 12/28/23.
 //
+// 파일명: NaverMap.swift
+// 기능: SwiftUI에서 네이버 지도를 표시하는 UIViewRepresentable 래퍼
+
 import SwiftUI
 import NMapsMap
 
 struct NaverMap: UIViewRepresentable {
-    
     func makeCoordinator() -> Coordinator {
         Coordinator.shared
     }
 
-    func makeUIView(context: Context) -> NMFNaverMapView {
+    func makeUIView(context: Context) -> NMFMapView {
         context.coordinator.getNaverMapView()
     }
 
-    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {}
+    func updateUIView(_ uiView: NMFMapView, context: Context) {
+        // 필요 시 지도 갱신 작업
+    }
 }
