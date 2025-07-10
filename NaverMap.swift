8e177a59330522e75fe9928eb441c3d25a48a044
oopss003 <jhpark8271@gmail.com>
2025-07-10 18:02:37 +0900

    Update NaverMap.swift
    "위치 찾기 성공"

--- NaverMap.swift
+++ NaverMap.swift
@@ -1,27 +1,32 @@
 //
-//  twopageApp.swift
+//  NaverMap.swift
 //  twopage
 //
 //  Created by jae on 7/8/25.
 //
 
 //
-//  NaverMapApp.swift
+//  NaverMap.swift
 //  NaverMap
 //
 //  Created by 황성진 on 12/28/23.
 //
+// 파일명: NaverMap.swift
+// 기능: SwiftUI에서 네이버 지도를 표시하는 UIViewRepresentable 래퍼
+
 import SwiftUI
-import FirebaseCore
+import NMapsMap
 
-@main
-struct twopageApp: App {
-    // Firebase 설정을 위해 AppDelegateAdaptor 사용
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+struct NaverMap: UIViewRepresentable {
+    func makeCoordinator() -> Coordinator {
+        Coordinator.shared
+    }
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+    func makeUIView(context: Context) -> NMFMapView {
+        context.coordinator.getNaverMapView()
+    }
+
+    func updateUIView(_ uiView: NMFMapView, context: Context) {
+        // 필요 시 지도 갱신 작업
     }
 }
