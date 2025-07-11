018cc2bb30bf155e074207d2e2cfffee94c012f7
oopss003 <jhpark8271@gmail.com>
2025-07-11 19:10:37 +0900

    Update ContentView.swift
    "지도 버튼 위치 변경"

--- ContentView.swift
+++ ContentView.swift
@@ -7,8 +7,8 @@
 // - FireStoreManager.swift (Firestore에서 위치 데이터 로딩)
 
 import SwiftUI
-import CoreLocationUI // LocationButton 사용
-import NMapsMap        // 네이버 지도
+import CoreLocationUI
+import NMapsMap
 
 struct ContentView: View {
     @StateObject var coordinator = Coordinator.shared
@@ -16,25 +16,29 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            VStack {
-                NaverMap()
-                    .ignoresSafeArea(.all, edges: .top)
-                    .onAppear {
-                        coordinator.checkIfLocationServiceIsEnabled()
-                    }
-                
-                Spacer()
-                
-                // 현재 위치로 이동하는 버튼
-                LocationButton(.currentLocation) {
-                    coordinator.updateMapWithLocation()
+            NaverMap()
+                .ignoresSafeArea(.all, edges: .top)
+                .onAppear {
+                    coordinator.checkIfLocationServiceIsEnabled()
                 }
-                .frame(width: 60, height: 60)
-                .cornerRadius(30)
-                .labelStyle(.iconOnly)
-                .symbolVariant(.fill)
-                .foregroundColor(.white)
-                .padding()
+            
+            // 지도 위 좌측 상단에 현재 위치 버튼
+            VStack {
+                HStack {
+                    LocationButton(.currentLocation) {
+                        coordinator.updateMapWithLocation()
+                    }
+                    .frame(width: 70, height: 70)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(35)
+                    .labelStyle(.iconOnly)
+                    .symbolVariant(.fill)
+                    .padding(.leading, 16)
+                    .padding(.top, 30)
+                    Spacer()
+                }
+                Spacer()
             }
         }
         .onAppear {
@@ -53,7 +57,8 @@ struct ContentView: View {
 }
 
 
-//
-//#Preview {
-//    ContentView()
-//}
+
+#Preview {
+    ContentView()
+}
+
